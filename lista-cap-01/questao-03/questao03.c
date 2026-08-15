b2f80c717e63de4c9e1698314f47c4bde88c0d49
Manoel Messias <manoelmpaz@gmail.com>
2026-08-15 08:27:11 -0300

    Adequando os comentários ao padrão ANSI C

--- lista-cap-01/questao-03/questao03.c
+++ lista-cap-01/questao-03/questao03.c
@@ -1,22 +1,22 @@
 /* Esse programa mostra o uso de comentários em várias linhas
    e mostra também o uso de comentários em uma única linha */
 
-// questao03.c
+/* questao03.c */
 
-#include <stdio.h> // Libera a utilização das funções padrão de entrada e saída de dados
+#include <stdio.h> /* Libera a utilização das funções padrão de entrada e saída de dados */
 
-int main() // Declarando a função main
-{ // Início do corpo da função main
+int main() /* Declarando a função main */
+{ /* Início do corpo da função main */
 
-    int a, b, c; // Declarando três variáveis do tipo inteiro: "a", "b" e "c"
+    int a, b, c; /* Declarando três variáveis do tipo inteiro: "a", "b" e "c" */
 
-    a = 1; // Atribuindo o valor numérico 1 à variável "a"
-    b = 2; // Atribuindo o valor numérico 2 à variável "b"
+    a = 1; /* Atribuindo o valor numérico 1 à variável "a" */
+    b = 2; /* Atribuindo o valor numérico 2 à variável "b" */
 
-    c = a + b; // Atribuindo o resultado da soma de "a" e "b" à variável "c"
+    c = a + b; /* Atribuindo o resultado da soma de "a" e "b" à variável "c" */
 
     printf("O resultado da soma entre %d e %d eh: %d", a, b, c); /* Mostra na tela o resultado da soma de
                                                                     "a" e "b", atribuída à variável "c" */
     return 0;
 
-} // Fim do corpo da função main
+} /* Fim do corpo da função main */
