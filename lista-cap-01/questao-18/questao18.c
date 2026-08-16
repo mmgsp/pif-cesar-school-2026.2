55d3231bb6842ccd92becbe2a1f29b6a2cf55d5f
Manoel Messias <manoelmpaz@gmail.com>
2026-08-16 00:07:42 -0300

    Questão 18

---
+++ lista-cap-01/questao-18/questao18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float lapis = 4.88;
+    float borrachas = 234.54;
+    float canetas = 42.04;
+    float cadernos = 8;
+    float fitas = 13.05;
+
+    printf("\n\nLapis%12.2f", lapis);
+    printf("\nBorrachas%12.2f", borrachas);
+    printf("\nCanetas%12.2f", canetas);
+    printf("\nCadernos%12.2f", cadernos);
+    printf("\nFitas%12.2f", fitas);
+
+    return 0;
+}
+
