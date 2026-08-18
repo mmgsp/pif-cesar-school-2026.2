183b4741534bece987bff4a2f8932a438d4afe48
Manoel Messias <manoelmpaz@gmail.com>
2026-08-18 14:25:55 -0300

    Questão 26

---
+++ lista-cap-01/questao-26/questao26.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("       X\n");
+    printf("      X*X\n");
+    printf("    X+XoX+X\n");
+    printf("   XoX+X*XoX\n");
+    printf("  X+XoXoXoX+X\n");
+    printf(" X*X+X*X+X*XXX\n");
+    printf("XXXXXXXXXXXXXXX\n");
+    printf("      XXX\n");
+    printf("      XXX\n");
+    printf("     XXXXX\n");
+
+    return 0;
+
+}
